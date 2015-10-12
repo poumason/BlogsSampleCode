07e6d4fca769fab0e3e0acd9dd5f52c1a0e9d6c4
poumason <poumason@live.com>
2015-10-12 09:57:03 +0800

    add adaptivemediasource sample

---
+++ DotblogsSampleCode/AdaptiveMediaSourceSample/BackgroundAudioTask/BackgroundAudioTask.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Media;
+using Windows.Media.Playback;
+using Windows.Media.Streaming.Adaptive;
+
+namespace BackgroundAudioTask
+{
+    public sealed class BackgroundAudioTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral deferral;
+        private SystemMediaTransportControls systemControl;
+        private AutoResetEvent BackgroundTaskStarted = new AutoResetEvent(false);
+        private AdaptiveMediaSource _source;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            deferral = taskInstance.GetDeferral();
+            systemControl = SystemMediaTransportControls.GetForCurrentView();
+
+            systemControl.ButtonPressed -= SystemControls_ButtonPressed;
+            systemControl.ButtonPressed += SystemControls_ButtonPressed;
+
+            taskInstance.Canceled -= TaskInstance_Canceled;
+            taskInstance.Canceled += TaskInstance_Canceled;
+            taskInstance.Task.Completed -= Task_Completed;
+            taskInstance.Task.Completed += Task_Completed;
+
+            BackgroundMediaPlayer.MessageReceivedFromForeground -= BackgroundMediaPlayer_MessageReceivedFromForeground;
+            BackgroundMediaPlayer.MessageReceivedFromForeground += BackgroundMediaPlayer_MessageReceivedFromForeground;
+
+            BackgroundTaskStarted.Set();
+        }
+
+        void Task_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            if (deferral != null)
+            {
+                deferral.Complete();
+            }
+        }
+
+        private void TaskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            BackgroundMediaPlayer.Shutdown();
+            if (deferral != null)
+            {
+                deferral.Complete();
+            }
+        }
+
+        void SystemControls_ButtonPressed(SystemMediaTransportControls sender, SystemMediaTransportControlsButtonPressedEventArgs args)
+        {
+        }
+
+        async void BackgroundMediaPlayer_MessageReceivedFromForeground(object sender, MediaPlayerDataReceivedEventArgs e)
+        {
+            foreach (string key in e.Data.Keys)
+            {
+                switch (key.ToLower())
+                {
+                    case "play":
+                        String url =e.Data[key].ToString();
+                        await PlayHLS(new Uri(url, UriKind.RelativeOrAbsolute));
+                        break;
+                    case "stop":
+                        BackgroundMediaPlayer.Current.Pause();
+                        break;
+                    case "wakup_background_player":
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private async Task PlayHLS(Uri hsl)
+        {
+            AdaptiveMediaSourceCreationResult result = await AdaptiveMediaSource.CreateFromUriAsync(hsl);
+            if (result.Status == AdaptiveMediaSourceCreationStatus.Success)
+            {
+                _source = result.MediaSource;
+                _source.DownloadRequested += _source_DownloadRequested;
+                _source.DownloadCompleted += _source_DownloadCompleted;
+                _source.DownloadFailed += _source_DownloadFailed;
+                _source.DownloadBitrateChanged += _source_DownloadBitrateChanged;
+                _source.PlaybackBitrateChanged += _source_PlaybackBitrateChanged;
+
+                BackgroundMediaPlayer.Current.SetMediaSource(result.MediaSource);
+            }
+        }
+
+        #region AdaptiveMediaSource Events
+        private void _source_PlaybackBitrateChanged(AdaptiveMediaSource sender, AdaptiveMediaSourcePlaybackBitrateChangedEventArgs args)
+        {
+
+        }
+
+        private void _source_DownloadBitrateChanged(AdaptiveMediaSource sender, AdaptiveMediaSourceDownloadBitrateChangedEventArgs args)
+        {
+
+        }
+
+        private void _source_DownloadFailed(AdaptiveMediaSource sender, AdaptiveMediaSourceDownloadFailedEventArgs args)
+        {
+
+        }
+
+        private void _source_DownloadCompleted(AdaptiveMediaSource sender, AdaptiveMediaSourceDownloadCompletedEventArgs args)
+        {
+        }
+
+        private void _source_DownloadRequested(AdaptiveMediaSource sender, AdaptiveMediaSourceDownloadRequestedEventArgs args)
+        {
+        }
+        #endregion
+    }
+}
---
+++ DotblogsSampleCode/AdaptiveMediaSourceSample/LiveStreamSample/MainPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+using Windows.Media.Playback;
+using Windows.Media.Streaming.Adaptive;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LiveStreamSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async Task<Uri> GetHitChannelHSL(String url, String tag)
+        {
+            Uri resultUri = null;
+            try
+            {
+                HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+                request.CookieContainer = new CookieContainer();
+                request.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+                request.AllowReadStreamBuffering = true;
+
+                var resposne = await request.GetResponseAsync();
+
+                using (StreamReader stream = new StreamReader(resposne.GetResponseStream()))
+                {
+                    while (stream.Peek() >= 0)
+                    {
+                        String strLine = stream.ReadLine();
+                        Debug.WriteLine(strLine);
+                        if (strLine.Contains(tag))
+                        {
+                            String[] para = strLine.Split('\'');
+                            String urlStr = para[1].Replace("\\", "");
+                            resultUri = new Uri(urlStr);
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //throw;
+            }
+            return resultUri;
+        }
+
+        private AdaptiveMediaSource _source;
+
+        private async void OnPlayClick(object sender, RoutedEventArgs e)
+        {
+            String url = txtURL.Text;
+            String tag = "ra000001";
+            Uri hsl = await GetHitChannelHSL(url, tag);
+            if (hsl != null)
+            {
+                // check is phone
+                bool isHardwareButtonsAPIPresent =
+        Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons");
+
+                if (isHardwareButtonsAPIPresent)
+                {
+                    //Windows.Phone.UI.Input.HardwareButtons.CameraPressed +=
+                    //    HardwareButtons_CameraPressed;
+                    ValueSet msg = new ValueSet();
+                    msg.Add("Play", hsl.OriginalString);
+                    BackgroundMediaPlayer.SendMessageToBackground(msg);
+                }
+
+                AdaptiveMediaSourceCreationResult result = await AdaptiveMediaSource.CreateFromUriAsync(hsl);
+                if (result.Status == AdaptiveMediaSourceCreationStatus.Success)
+                {
+                    _source = result.MediaSource;
+                    _source.DownloadRequested += _source_DownloadRequested;
+                    _source.DownloadCompleted += _source_DownloadCompleted;
+                    _source.DownloadFailed += _source_DownloadFailed;
+                    _source.DownloadBitrateChanged += _source_DownloadBitrateChanged;
+                    _source.PlaybackBitrateChanged += _source_PlaybackBitrateChanged;
+
+                    mediaPlayer.SetMediaStreamSource(result.MediaSource);
+                }
+            }
+        }
+
+        private void _source_PlaybackBitrateChanged(AdaptiveMediaSource sender, AdaptiveMediaSourcePlaybackBitrateChangedEventArgs args)
+        {
+         
+        }
+
+        private void _source_DownloadBitrateChanged(AdaptiveMediaSource sender, AdaptiveMediaSourceDownloadBitrateChangedEventArgs args)
+        {
+
+        }
+
+        private void _source_DownloadFailed(AdaptiveMediaSource sender, AdaptiveMediaSourceDownloadFailedEventArgs args)
+        {
+            
+        }
+
+        private void _source_DownloadCompleted(AdaptiveMediaSource sender, AdaptiveMediaSourceDownloadCompletedEventArgs args)
+        {
+        }
+
+        private void _source_DownloadRequested(AdaptiveMediaSource sender, AdaptiveMediaSourceDownloadRequestedEventArgs args)
+        {
+        }
+
+        private void OnPauseClick(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+    }
+}
