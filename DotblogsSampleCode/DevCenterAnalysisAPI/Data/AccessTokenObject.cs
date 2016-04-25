deddb5c82f72eb1a6ee2f6170ac2cc3a82661d32
Pou Mason <Pou Mason>
2016-04-26 00:47:23 +0800

    add DevCenterAnalysisAPI project

---
+++ DotblogsSampleCode/DevCenterAnalysisAPI/Data/AccessTokenObject.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCenterAnalysisAPI
+{
+    public class AccessTokenObject
+    {
+        [JsonProperty("token_type")]
+        public string token_type { get; set; }
+
+        [JsonProperty("expires_in")]
+        public string ExpliresIn { get; set; }
+
+        [JsonProperty("expires_on")]
+        public string ExpiresOn { get; set; }
+
+        [JsonProperty("not_before")]
+        public string NotBefore { get; set; }
+
+        [JsonProperty("resource")]
+        public string Resource { get; set; }
+
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+    }
+}
---
+++ DotblogsSampleCode/DevCenterAnalysisAPI/Data/ReviewData.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCenterAnalysisAPI
+{
+    public class AnalysisData<T>
+        where T: class
+    {
+        [JsonProperty("Value")]
+        public T[] Value { get; set; }
+
+        [JsonProperty("nextLink")]
+        public string nextLink { get; set; }
+
+        [JsonProperty("TotalCount")]
+        public int TotalCount { get; set; }
+    }
+
+    public class ReviewData
+    {
+        [JsonProperty("date")]
+        public string Date { get; set; }
+        public string applicationId { get; set; }
+        public string applicationName { get; set; }
+        public string market { get; set; }
+        public string osVersion { get; set; }
+        public string deviceType { get; set; }
+        public bool isRevised { get; set; }
+        public string packageVersion { get; set; }
+        public string deviceModel { get; set; }
+        public string productFamily { get; set; }
+        public int deviceRAM { get; set; }
+        public string deviceScreenResolution { get; set; }
+        public int deviceStorageCapacity { get; set; }
+        public bool isTouchEnabled { get; set; }
+        public string reviewerName { get; set; }
+        public int rating { get; set; }
+        public string reviewTitle { get; set; }
+
+        [JsonProperty("reviewText")]
+        public string ReviewText { get; set; }
+        public int helpfulCount { get; set; }
+        public int notHelpfulCount { get; set; }
+        public object responseDate { get; set; }
+        public string responseText { get; set; }
+    }
+
+}
