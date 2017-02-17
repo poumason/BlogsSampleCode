b6fb67e7eaf268717ef75938fff8859d5e1e5f2f
pouma <pouma@DESKTOP-D9FAMPF>
2017-02-17 20:45:58 +0800

    modify new sample folder structure

---
+++ DotblogsSampleCode/14-InAppOnsSample/InAppOnsSample/DataWrapper/UserDataWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace InAppOnsSample
+{
+    public class UserDataWrapper
+    {
+        public User Self { get; private set; }
+
+        public string Id { get; private set; }
+
+        public UserDataWrapper(User user, string id)
+        {
+            Self = user;
+            Id = id;
+        }
+    }
+}
---
+++ DotblogsSampleCode/14-InAppOnsSample/InAppOnsSample/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Services.Store;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace InAppOnsSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPageViewModel mainPageViewModel { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            mainPageViewModel = new MainPageViewModel();
+
+            DataContext = mainPageViewModel;
+
+            Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await mainPageViewModel.Initialize();
+        }
+    }
+}
