f961a73ea31bbf90ef21b71e16c033d3fa9b40e5
pouma <pouma@DESKTOP-D9FAMPF>
2017-03-06 16:41:24 +0800

    add double mediaplayer test code

--- DotblogsSampleCode/02-BackgroundMediaPlayerSample/SingleBackgroundMediaPlayer/MainPage.xaml.cs
+++ DotblogsSampleCode/02-BackgroundMediaPlayerSample/SingleBackgroundMediaPlayer/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -66,5 +67,33 @@ namespace SingleBackgroundMediaPlayer
             PlayerService.Instance.PlaybackList = ViewModel.MediaPlaybackList;
             PlayerService.Instance.Play();
         }
+
+        private void DoubleMediaPlayer()
+        {
+            var list = ViewModel.MediaPlaybackList;
+            var list1 = new MediaPlaybackList();
+            var list2 = new MediaPlaybackList();
+
+            for (int i = 0; i < list.Items.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    list1.Items.Add(list.Items[i]);
+                }
+                else
+                {
+                    list2.Items.Add(list.Items[i]);
+                }
+            }
+
+            player1.PlaybackList = list1;
+            player2.PlaybackList = list2;
+
+            player1.Play();
+            player2.Play();
+        }
+
+        PlayerService player1 = new PlayerService();
+        PlayerService player2 = new PlayerService();
     }
 }
