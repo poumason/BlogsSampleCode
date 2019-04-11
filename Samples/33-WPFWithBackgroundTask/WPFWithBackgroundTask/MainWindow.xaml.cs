6ef17eac5bb070e06ecbc011c0663e2034f81053
Pou Lin <poumason@live.com>
2019-04-12 00:02:34 +0800

    add 33,34 project

---
+++ Samples/33-WPFWithBackgroundTask/WPFWithBackgroundTask/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.ApplicationModel.Background;
+
+namespace WPFWithBackgroundTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            RegistBackgroundTask();
+        }
+
+        private void RegistBackgroundTask()
+        {
+            var taskRegistered = false;
+            var exampleTaskName = "ExampleBackgroundTask";
+
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == exampleTaskName)
+                {
+                    taskRegistered = true;
+                    break;
+                }
+            }
+
+            if (taskRegistered)
+            {
+                return;
+            }
+
+            var builder = new BackgroundTaskBuilder();
+
+            builder.Name = exampleTaskName;
+            builder.TaskEntryPoint = "MyBackgroundTask.ExampleBackgroundTask";
+            builder.SetTrigger(new SystemTrigger(SystemTriggerType.TimeZoneChange, false));
+            builder.AddCondition(new SystemCondition(SystemConditionType.UserPresent));
+            BackgroundTaskRegistration registResult = builder.Register();
+        }
+    }
+}
